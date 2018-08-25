ea977ef8cc83a054e9c934e1e85a65001c4306de
Jacob Shaw <jds98929@uga.edu>
2018-08-25 15:37:22 -0400

    Finished assignment

--- Javascript Packet/packet.js
+++ Javascript Packet/packet.js
@@ -249,26 +249,74 @@ function Person(name, age){
 // Define function getUSA()
 // Find the html element that contains "USA".
 // Print that element's contents.
-  
+function getUSA(){
+    let els = document.getElementsByTagName('span');
+    for ( let i = els.length; i--; ) {
+        if (els[i].innerText === 'USA'){
+            console.log(els[i].innerText);
+        }
+    }
+}  
+
+
 // 2. Sales
 // Define function getPeopleInSales()
 // Print the names of all the people in the sales department.
-  
+function getPeopleInSales(){
+    let els = document.getElementsByTagName('td');
+    for ( let i = els.length; i--; ) {
+        if (els[i].innerText === 'Sales'){
+            console.log(els[i-1].innerText);
+        }
+    }
+}  
+
 // 3. Click Here
 // Define function getAnchorChildren()
 // Find all anchor elements with a <span> child.
 // Print the contents of <span>
+function getAnchorChildren(){
+    let els = document.getElementsByTagName('a');
+    for ( let i = els.length; i--; ) {
+        if (els[i].getElementsByTagName('span')){
+            let span_els = els[i].getElementsByTagName('span');
+            for ( let i = span_els.length; i--; ) {
+                console.log(span_els[i].innerText);
+            }
+        }
+    }
+}
   
 // 4. Hobbies
 // Define function getHobbies()
 // Find all checked options in the 'skills' select element.
 // Print the value and the contents.
+function getHobbies(){
+    let skills = document.getElementsByName('skills');
+    let els = skills[0].getElementsByTagName('option');
+    for (let i = 0; i < els.length; i++){
+        if(els[i].getAttributeNode('selected')){
+            if( els[i].getAttributeNode('selected').nodeValue === 'selected'){
+                console.log(els[i].innerHTML);
+            }
+        }
+    }    
+}
   
 // 5. Custom Attribute
 // Define function getCustomAttribute()
 // Find all elements with "data-customAttr" attribute
 // Print the value of the attribute.
 // Print the element that has the attribute.
+function getCustomAttribute(){
+    let els= document.getElementsByTagName('*');
+    for (let i = 0; i < els.length; i++){
+        if (els[i].getAttributeNode('data-customAttr')){
+            console.log(els[i].innerHTML);
+            console.log(els[i].getAttributeNode('data-customAttr').nodeValue)
+        }
+    }
+}
 
 // 6. Sum Event
 // NOTE: Write unobtrusive Javascript
@@ -282,11 +330,48 @@ function Person(name, age){
 // Put the sum in the <span> element.
 // If values cannot be added, put "Cannot add" in the <span> element
 
+document.getElementById('num1').addEventListener('change', function(event){
+    event.preventDefault();
+    let total = 0;
+    let op1 = parseInt(document.getElementById('num1').value);
+    let op2 = parseInt(document.getElementById('num2').value);
+    if(op2){
+        total = op1 + op2;
+    } else {
+        total = op1;
+    }
+    document.getElementById('sum').innerText = total;
+});
+document.getElementById('num2').addEventListener('change', function(event){
+    event.preventDefault();
+    let total = 0;
+    let op2 = parseInt(document.getElementById('num2').value);
+    let op1 = parseInt(document.getElementById('num1').value);
+    if(op1){
+        total  = op1 + op2;
+    } else {
+        total = op2;
+    }
+    document.getElementById('sum').innerText = total;
+});
+
+
 // 7. Skills Event
 // NOTE: Write unobtrusive Javascript
 // When user selects a skill, create an alert with a message similar to:
 // 	"Are you sure CSS is one of your skills?"
 // NOTE: no alert should appear when user deselects a skill.
+
+let els = document.getElementsByName('skills');
+els[0].addEventListener('click', function(event){
+    event.preventDefault();
+    if(event.target.getAttributeNode('selected')){
+        event.target.removeAttribute('selected');
+    } else {
+        alert(`Are you sure ${event.target.value} is one of your skills?`);
+        event.target.setAttribute('selected', 'selected');
+    }
+});
 
 // 8. Favorite Color Event
 // NOTE: Write unobtrusive Javascript
@@ -295,12 +380,35 @@ function Person(name, age){
 // 	"So you like green more than blue now?"
 // In this example, green is the new value and blue is the old value.
 // Make the background color (of all favoriteColor radio buttons) the newly selected favoriteColor
-
+let clicks = new Array();
+clicks[0] = 'none';
+fc = document.getElementsByName('favoriteColor');
+for(let i = 0; i < fc.length; i++){
+    fc[i].addEventListener('click', function(event){
+        event.preventDefault;
+        clicks.push(event.target.value);
+        if (clicks[clicks.length-2] !== 'none'){
+            alert(`So you like ${[event.target.value]} more than ${clicks[clicks.length-2]} now?`)
+        }
+        return clicks;
+    })
+}
 // 9. Show/Hide Event
 // NOTE: Write unobtrusive Javascript
 // When user hovers over an employees name:
 // 	Hide the name if shown.
 // 	Show the name if hidden.
+let emps = document.getElementsByClassName('empName');
+for (let i = 0; i < emps.length; i++){
+    emps[i].style.visibility = 'visible';
+    emps[i].addEventListener('mouseover', function(event){
+        if (emps[i].style.visibility === 'visible'){
+            emps[i].style.visibility = 'hidden';
+        } else if (emps[i].style.visibility === 'hidden'){
+            emps[i].style.visibility = 'visible';
+        }
+    });
+}
 
 // 10. Current Time
 // Regarding this element:
@@ -308,13 +416,65 @@ function Person(name, age){
 // Show the current time in this element in this format: 9:05:23 AM
 // The time should be accurate to the second without having to reload the page.
 
+function checkTime(i) {
+    if (i < 10) {
+      i = "0" + i;
+    }
+    return i;
+  }
+  
+  function startTime() {
+    let today = new Date();
+    let h = today.getHours();
+    let m = today.getMinutes();
+    let s = today.getSeconds();
+    m = checkTime(m);
+    s = checkTime(s);
+    document.getElementById('currentTime').innerHTML = h + ":" + m + ":" + s;
+    t = setTimeout(function() {
+      startTime()
+    }, 500);
+  }
+  startTime();
+
 // 11. Delay
 // Regarding this element:
 // 	<p id="helloWorld">Hello, World!</p>
 // Three seconds after a user clicks on this element, change the text to a random color.
+hw = document.getElementById('helloWorld');
+hw.addEventListener('click', function(event){
+    event.preventDefault();
+    setTimeout(function(){
+        hw.style.color = getRandomColor();
+    },  3000);
+});
 
+
+function getRandomColor() {
+    let letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  }
 // 12. Walk the DOM
 // Define function walkTheDOM(node, func)
 // This function should traverse every node in the DOM. Use recursion.
 // On each node, call func(node).
+
+function walkTheDOM(node, func){
+    func(node);
+    node = node.firstChild;
+      while(node)
+      {
+        walkTheDOM(node, func);
+        node = node.nextSibling;
+      }
+}
+
+walkTheDOM(document.body,
+    function(node){
+        console.log(node.innerHTML);
+});
 
